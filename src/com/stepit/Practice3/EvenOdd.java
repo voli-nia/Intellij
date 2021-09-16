50d251d8f01d3baa9358d226cfb7ff57a97089be
voli_nia <volievscaia@gmail.com>
2021-09-16 21:55:14 +0300

    L5HW Calculator

---
+++ src/com/stepit/Practice3/EvenOdd.java
@@ -0,0 +1,17 @@
+package com.stepit.Practice3;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    static boolean isEven(int number) {
+        return number %2 == 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter your number: ");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        if (isEven(num)) {
+            System.out.println("Number is even");
+        } else System.out.println("Number is odd");
+    }
+}
