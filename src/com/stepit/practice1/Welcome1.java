66f35f17e2a8bd54839bcd81e8548258c2c400ce
voli-nia <88889942+voli-nia@users.noreply.github.com>
2021-09-16 22:23:43 +0300

    Delete Welcome1.java

--- src/com/stepit/practice1/Welcome1.java
+++
@@ -1,9 +0,0 @@
-// Text-printing program.
-package com.stepit.practice1;
-public class Welcome1 {
-    // main method begins execution of Java application
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome to Java Programming!");
-    } // end method main
-} // end class Welcome1
